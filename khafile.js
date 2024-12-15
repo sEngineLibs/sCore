2e6dc070aa3329652c22b2873f095dd855e7dab9
smoothie-ws <kibot.email@gmail.com>
2024-12-15 12:49:03 +1000

    Remove sEngine assets

--- khafile.js
+++ khafile.js
@@ -1,9 +1,5 @@
 let project = new Project("New Project");
-
 project.addAssets("Assets/**");
 project.addShaders("Shaders/**");
 project.addSources("Sources");
-
-project.addDefine("SUI_DEBUG_FPS");
-
 resolve(project);
